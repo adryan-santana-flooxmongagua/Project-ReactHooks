6836864e890649c333eadf00215efe62e1d6271b
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-19 10:04:17 -0300

    feat: memoize computed values using useMemo

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,58 +1,47 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
 
-const merge = function(s1, s2) {
-    return [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("")
+function sum(a, b) {
+    const future = Date.now() + 2000
+    while(Date.now() < future) {} // espera... 2s
+    return a + b
 }
 
-const UseRef = props => {
-    const [value1, setValue1] = useState("")
-    const [value2, setValue2] = useState("")
-    
-    const count = useRef(0)
-    const myInput1 = useRef(null)
-    const myInput2 = useRef(null)
+const UseMemo = props => {
+    const [n1, setN1] = useState(0)
+    const [n2, setN2] = useState(0)
+    const [n3, setN3] = useState(0)
 
-    useEffect(function() {
-        count.current = count.current + 1
-        myInput2.current.focus()
-    }, [value1])
-    
-    useEffect(function() {
-        count.current++
-        myInput1.current.focus()
-    }, [value2])
+    const result = useMemo(() => sum(n1, n2), [n1, n2])
 
+// opção alternativa
+
+/* const [result, setResult] = useState(0);
+
+  useEffect(
+    function () {
+      setResult(sum(n1, n2));
+    },
+    [n1, n2]);
+*/
 
     return (
-        <div className="UseRef">
+        <div className="UseMemo">
             <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!" />
-    
-            <SectionTitle title="Exercício #01" />
+                title="Hook UseMemo"
+                subtitle="Retorna um valor memoizado!" />
+            
             <div className="center">
-                <div>
-                    <span className="text">Valor: </span>
-                    <span className="text">{merge(value1, value2)} [</span>
-                    <span className="text red">{count.current}</span>
-                    <span className="text">]</span>
-                </div>
-
-                <input type="text" className="input" 
-                    ref={myInput1}
-                    value={value1} onChange={e => setValue1(e.target.value)} />
-            </div>
-
-            <SectionTitle title="Exercício #02" />
-            <div className="center">
-                <input type="text" className="input"
-                    ref={myInput2}
-                    value={value2} onChange={e => setValue2(e.target.value)}/>
+                <input type="number" className="input"
+                    value={n1} onChange={e => setN1(parseInt(e.target.value))}/>
+                <input type="number" className="input"
+                    value={n2} onChange={e => setN2(parseInt(e.target.value))}/>
+                <input type="number" className="input"
+                    value={n3} onChange={e => setN3(parseInt(e.target.value))}/>
+                <span className="text">{result}</span>
             </div>
         </div>
     )
 }
 
-export default UseRef
+export default UseMemo
